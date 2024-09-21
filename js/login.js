d2b9ee44c4f38f88767b9fc30a539e039bc726d4
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-09-21 12:55:42 +0600

    log in Problems

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('button-login').addEventListener('click', function(event
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
     if(phoneNumber === '01409012843' && pinNumber === '1234' ) {
-        window.location.href = '/home.html';
+        window.location.href = './home.html';
     }
     else{
         alert('Your Number is 01409012843 & Your pin 1234')
